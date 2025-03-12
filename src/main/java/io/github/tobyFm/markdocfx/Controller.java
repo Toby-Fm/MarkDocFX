a7b521126fd3e4e2a493242c8fcf25544b74537b
Toby-Fm <mail@toby-wichmann.de>
2025-03-12 22:29:47 +0100

    ruft einzelne .md Datei auf und zeigt sie im TreeView an

--- src/main/java/io/github/tobyFm/markdocfx/Controller.java
+++ src/main/java/io/github/tobyFm/markdocfx/Controller.java
@@ -1,16 +1,24 @@
 package io.github.tobyFm.markdocfx;
 
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Controller {
 
     @FXML
-    private TreeView<?> fileTreeView;
+    private TreeView<String> fileTreeView;
 
     @FXML
     private AnchorPane fileViewInput;
@@ -27,4 +35,38 @@ public class Controller {
     @FXML
     private TabPane tabPane;
 
+    @FXML
+    private MFXButton openFile;
+
+    @FXML
+    private MFXButton openFolder;
+
+    @FXML
+    private MFXButton closeAll;
+
+    // -------------------------------------------
+    // Öffnet einzelne Datei
+    // -------------------------------------------
+    public void openFile(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("File Explorer");
+        // Nimmt nur .md Dateien.
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Markdown Files", "*.md"));
+        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        File file = fileChooser.showOpenDialog(stage);
+
+        if (file != null) {
+            System.out.println("DEBUG: Der Path von der Datei ist folgender : " + file.getAbsolutePath());
+            System.out.println("DEBUG: Der name der Datei ist folgender : " + file.getName());
+
+            String fileName = file.getName();
+
+            // Fügt Datei zu dem TreeView hinzu bzw. zeigt sie an.
+            final TreeItem<String> fileList = new TreeItem("/");
+            fileList.getChildren().add(new TreeItem<>(fileName));
+            fileTreeView.setRoot(fileList);
+        } else {
+            System.out.println("No file selected");
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,4 @@
 module com.example.markdocfx {
-    requires javafx.controls;
-    requires javafx.fxml;
     requires javafx.web;
 
     requires org.controlsfx.controls;
@@ -8,7 +6,7 @@ module com.example.markdocfx {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires java.desktop;
+    requires MaterialFX;
 
     opens io.github.tobyFm.markdocfx to javafx.fxml;
     exports io.github.tobyFm.markdocfx;
